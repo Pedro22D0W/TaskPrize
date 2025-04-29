3b2d8a0811cd842e6ad3049d89a5e615e3ae9c73
Zaldef <guilhermeroelli@gmail.com>
2025-04-28 22:55:01 -0300

    Create CORS configuration and security settings to allow all origins and methods

--- TaskPrizeAPI/src/main/java/com/taskprize/controller/AuthController.java
+++ TaskPrizeAPI/src/main/java/com/taskprize/controller/AuthController.java
@@ -18,8 +18,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Optional;
-
-@CrossOrigin(origins = "http://localhost:8100")
 @RestController
 @RequestMapping("/auth")
 @RequiredArgsConstructor
